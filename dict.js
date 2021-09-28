8eae4cd2f9b9501bd4e40ffa3cc8f9d65a295da6
dzmitryNz <d.nayezzhy@gmail.com>
2021-09-29 00:08:32 +0300

    feat: dictonary

---
+++ dict.js
@@ -0,0 +1,24 @@
+export const en = { 
+  weekdays: [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 
+  'Thursday', 'Friday', 'Saturday'],
+  months: ['January', 'February', 'March', 'April', 'May', 'June', 
+  'July', 'August', 'September', 'October', 'November', 'December'],
+  greetings: ['Good morning', 'Good day', 'Good evening', 'Good night'],
+  name: 'Your name here',
+  focus: 'Here is the important thing',
+  city: 'Belarus',
+  humidity:'humidity',
+  wind: 'wind speed',
+}
+export const ru = { 
+  weekdays: [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 
+  'Thursday', 'Friday', 'Saturday'],
+  months: ['January', 'February', 'March', 'April', 'May', 'June', 
+  'July', 'August', 'September', 'October', 'November', 'December'],
+  greetings: ['Good morning', 'Good day', 'Good evening', 'Good night'],
+  name: 'Your name here',
+  focus: 'Here is the important thing',
+  city: 'Belarus',
+  humidity:'humidity',
+  wind: 'wind speed',
+}
